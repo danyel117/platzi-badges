a279a4d59a725f65d81af56b67bda2833d6f2f0a
danyel117 <dsaldarriaga@prevalentware.com>
2020-08-10 12:08:29 -0500

    agregar funcionalidad para cambiar badges segun datos del form

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -21,10 +21,10 @@ const BadgeNew = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-6">
-                        <Badge first_name={"Daniel"} last_name={"Saldarriaga"} twitter={"@Daniel_Code"} job_title={"Frontend Engineer"} avatar_url={"https://s.gravatar.com/avatar/cf13f80b8c3936bb96a999b9fc9c48e6?s=80"} /> 
+                        <Badge first_name={formData.first_name} last_name={formData.last_name} twitter={formData.twitter} job_title={formData.job_title} email={formData.email} avatar_url={"https://s.gravatar.com/avatar/cf13f80b8c3936bb96a999b9fc9c48e6?s=80"} /> 
                     </div>
                     <div className="col-6">
-                        <BadgeForm onChange={handleChange} formData={formData}/>
+                        <BadgeForm handleChange={handleChange} formData={formData}/>
                     </div>
                 </div>
             </div>
